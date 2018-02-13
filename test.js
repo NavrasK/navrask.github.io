d854aaf6848a1ced67a6552de287ae67d5b94b07
Navras Kamal <32576299+NavrasK@users.noreply.github.com>
2018-02-13 10:48:19 -0700

    working on drawing functions

--- test.js
+++ test.js
@@ -1,7 +1,30 @@
-var c = document.getElementById("c");
-var ctx = c.getContext("2d");
+var c, ctx, xmax, ymax;
 
-function randDegrees(){
-    return Math.floor((Math.random() * 20) + -20);
+window.onload = init;
+
+function init(){
+    var c = document.getElementById("c");
+    var ctx = c.getContext("2d");
+    var xmax = window.innerWidth;
+    var ymax = window.innerHeight;
+
 }
 
+var numElements = 6;
+
+function randDegrees(ang){
+    return Math.floor((Math.random() * (ang+20)) + ang-20);
+}
+
+function center(){
+    return xmax/2, ymax/2;
+}
+
+function drawCracks(){
+    ctx.moveTo(center());
+    ctx.lineTo(xmax+20,ymax+20);
+    ctx.stroke();
+    for (var i = 0; i < numElements; i++){
+        angle = 360/numElements;
+    }
+}
